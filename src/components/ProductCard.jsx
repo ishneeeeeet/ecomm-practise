b25be9cacec5497679d979f70603da0fe6a3d41d
Ishneet Singh <ishneet100@gmail.com>
2023-07-04 14:16:05 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   src/components/ProductCard.jsx
    	modified:   src/pages/Home.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { cartContext } from "../context/context";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const { addToCart } = useContext(cartContext);
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div key={product.id} className="rounded-md border">
+      <img
+        src={product.image}
+        alt="Laptop"
+        className="aspect-[16/9] w-full rounded-md md:aspect-auto md:h-[300px] lg:h-[200px]"
+      />
+      <div className="p-4">
+        <h1 className="inline-flex items-center text-lg font-semibold">
+          {product.title}
+        </h1>
+        
+        
+        <div className="mt-3 flex items-center space-x-2">
+          <span className="block text-sm font-semibold">
+            Price: {product.price}
+          </span>
+        </div>
+        <button
+          onClick={() => addToCart(product)}
+          type="button"
+          className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { ChevronDown, Filter, LayoutGrid, Plus } from "lucide-react";
 import { cartContext } from "../context/context";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const { cart, addToCart } = useContext(cartContext);
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   useEffect(() => {
     const fetchDetails = async () => {
       const response = await axios.get("https://fakestoreapi.com/products");
@@ -15,60 +18,42 @@ const Home = () => {
     };
     fetchDetails();
   }, []);
+
+  useEffect(() => {
+    const filteredArray = products.filter((product) =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredProducts(filteredArray);
+  }, [search, products]);
+
   return (
     <>
+      <div className="w-full md:w-1/3">
+        <label
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor="name"
+        >
+          Enter your product Name
+        </label>
+        <input
+          className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+          type="text"
+          placeholder="search"
+          id="name"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
-        {products.map((i) => (
-          <div key={i} className="rounded-md border">
-            <img
-              src={i.image}
-              alt="Laptop"
-              className="aspect-[16/9] w-full rounded-md md:aspect-auto md:h-[300px] lg:h-[200px]"
-            />
-            <div className="p-4">
-              <h1 className="inline-flex items-center text-lg font-semibold">
-                {i.title}
-              </h1>
-              <p className="mt-3 text-sm text-gray-600">{i.description}</p>
-              <div className="mt-4">
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Sneakers
-                </span>
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Nike
-                </span>
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Airmax
-                </span>
-              </div>
-              <div className="mt-3 flex items-center space-x-2">
-                <span className="block text-sm font-semibold">Colors : </span>
-                <span className="block h-4 w-4 rounded-full border-2 border-gray-300 bg-red-400"></span>
-                <span className="block h-4 w-4 rounded-full border-2 border-gray-300 bg-purple-400"></span>
-                <span className="block h-4 w-4 rounded-full border-2 border-gray-300 bg-orange-400"></span>
-              </div>
-              <div className="mt-5 flex items-center space-x-2">
-                <span className="block text-sm font-semibold">Size : </span>
-                <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
-                  8 UK
-                </span>
-                <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
-                  9 UK
-                </span>
-                <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
-                  10 UK
-                </span>
-              </div>
-              <button
-                onClick={() => addToCart(i)}
-                type="button"
-                className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        ))}
+        {filteredProducts.length === 0 ? (
+          products.map((product) => (
+            <ProductCard product={product}/>
+          ))
+        ) : (
+          filteredProducts.map((product) => (
+            <ProductCard product={product}/>
+          ))
+        )}
       </div>
     </>
   );
